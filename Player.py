bb909ffdd68b18ff8d9b571faa517e7f1d53b967
caronorton <cnorton@potomacschool.org>
2022-10-12 08:24:35 -0400

    This is being added

--- Player.py
+++ Player.py
@@ -1,5 +1,10 @@
 
-def __init__(Player 1, Player 2):
-    self.hours = h
-    self.minutes = m
-    self.seconds = s
+def __init__(self, player1, player2):
+    self.p1 = player1
+    self.p2 = player2
+
+def getPlayer1(self):
+    return self.p1
+
+def getPlayer2(self):
+    return self.p2
