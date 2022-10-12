df7d6dbbd1aeca133885b34d8a1cd84552ac2d81
aldensmallwood <asmallwood@potomacschool.org>
2022-10-12 08:35:12 -0400

    Initial Commit

--- Player.py
+++ Player.py
@@ -1,12 +1,9 @@
 
-def __init__(self, player1, player2):
-    self.p1 = player1
-    self.p2 = player2
+def __init__(self, name):
+    self.p1 = name
 
-def getPlayer1(self): #comment
+
+def getName(self):
     return self.p1
 
-def getPlayer2(self): #comment
-    return self.p2
 
-# changes
