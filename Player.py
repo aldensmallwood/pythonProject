c6a52634d9cb4978d6bb675d8be043a14c9b64dc
caronorton <cnorton@potomacschool.org>
2022-10-12 08:29:42 -0400

    This is being added

--- Player.py
+++ Player.py
@@ -3,10 +3,10 @@ def __init__(self, player1, player2):
     self.p1 = player1
     self.p2 = player2
 
-def getPlayer1(self):
+def getPlayer1(self): #comment
     return self.p1
 
-def getPlayer2(self):
+def getPlayer2(self): #comment
     return self.p2
 
 # changes
