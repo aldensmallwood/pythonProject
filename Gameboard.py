5814bb13c74a83d3b3259326d9e5dd34977845ae
aldensmallwood <asmallwood@potomacschool.org>
2022-10-14 08:44:13 -0400

    Initial Commit

--- Gameboard.py
+++ Gameboard.py
@@ -9,14 +9,17 @@ class Gameboard:
 
     def onePlay(self, p1, oneOrTwo):
         pockStart = p1.pickPocket()
-        currentPock = pockStart
         seeds = self.board[pockStart]
+        self.board[pockStart] = 0
+        currentPock = pockStart
         for x in seeds:
-            if (self.board[currentPock] == 0 and oneOrTwo == 1) or (self.board[currentPock] == 7 and oneOrTwo == 2):
+            if (currentPock == 0 and oneOrTwo == 1) or (currentPock == 7 and oneOrTwo == 2):
                 self.board[currentPock] = self.board[currentPock] + 1
-            elif (self.board[currentPock] == 0 and oneOrtwo == 2) or (self.board[currentPock] == 7 and oneOrtwo == 1):
                 currentPock = currentPock + 1
-            elif (self.board[currentPock] == 13):
+            elif (currentPock == 0 and oneOrtwo == 2) or (currentPock == 7 and oneOrtwo == 1):
+                currentPock = currentPock + 1
+                x = x + 1
+            elif currentPock == 13:
                 self.board[currentPock] = self.board[currentPock] + 1
                 currentPock = 0
             else:
@@ -25,7 +28,8 @@ class Gameboard:
 
             # also need a variable for the last pocket added 
 
-
+    def displayBoard(self):
+        print(self.board[0 , 7])
 
 
 
