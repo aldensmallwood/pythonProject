907452354b9c5f3f7dea2f8fafd0018f0fdecbe1
aldensmallwood <asmallwood@potomacschool.org>
2022-10-18 08:36:14 -0400

    Edits

--- Gameboard.py
+++ Gameboard.py
@@ -6,32 +6,43 @@ class Gameboard:
 
     def __init__(self):
         self.board = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4]
+        self.currentPock = 0
 
     def onePlay(self, p1, oneOrTwo):
         ot = oneOrTwo
         pockStart = p1.pickPocket()
         seeds = self.board[pockStart]
         self.board[pockStart] = 0
-        currentPock = pockStart+1
+        self.currentPock = pockStart + 1
         print("seeds=",seeds)
         for x in range(seeds):
-            if (currentPock == 0 and ot == 1) or (currentPock == 7 and ot == 2):
-                self.board[currentPock] = self.board[currentPock] + 1
-                currentPock = currentPock + 1
-            elif (currentPock == 0 and ot == 2) or (currentPock == 7 and ot == 1):
-                currentPock = currentPock + 1
-                x = x-1
-            elif currentPock == 13:
-                self.board[currentPock] = self.board[currentPock] + 1
-                currentPock = 0
+            if (self.currentPock == 0 and ot == 1) or (self.currentPock == 7 and ot == 2):
+                self.board[self.currentPock] = self.board[self.currentPock] + 1
+                self.currentPock = self.currentPock + 1
+            elif (self.currentPock == 0 and ot == 2) or (self.currentPock == 7 and ot == 1):
+                self. currentPock = self.currentPock + 1
+                self.board[self.currentPock] = self.board[self.currentPock] + 1
+                self.currentPock = self.currentPock + 1
+            elif self.currentPock == 13:
+                self.board[self.currentPock] = self.board[self.currentPock] + 1
+                self.currentPock = 0
             else:
-                self.board[currentPock] = self.board[currentPock] + 1
-                currentPock = currentPock + 1
+                self.board[self.currentPock] = self.board[self.currentPock] + 1
+                self.currentPock = self.currentPock + 1
 
             # also need a variable for the last pocket added 
 
     def displayBoard(self):
-        print(self.board)
+        print(" ", self.board[1], self.board[2], self.board[3], self.board[4], self.board[5], self.board[6])
+        print(self.board[0], "           ", self.board[7])
+        print(" ", self.board[13], self.board[12], self.board[11], self.board[10], self.board[9], self.board[8])
+
+
+
+
+
+
+
 
 
 
--- MancalaClient.py
+++ MancalaClient.py
@@ -3,7 +3,6 @@ from Gameboard import Gameboard
 
 p1 = Human("Alden", 1)
 g1 = Gameboard()
-pock = p1.pickPocket()
 print(g1.displayBoard())
 g1.onePlay(p1, 1)
 print(g1.displayBoard())
