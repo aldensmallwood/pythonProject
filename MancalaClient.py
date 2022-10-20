720be5bcdbad43f0b0b2dce3a82aa091c3df849f
caronorton <cnorton@potomacschool.org>
2022-10-20 09:27:32 -0400

    This is being added

--- MancalaClient.py
+++ MancalaClient.py
@@ -6,10 +6,17 @@ p2 = Human("Caroline", 2)
 
 g1 = Gameboard()
 print(g1.displayBoard())
-g1.onePlay(p1, 1)
 
-g1.onePlay(p2, 2)
-print(g1.displayBoard())
-g1.determineWin()
+while g1.determineWin() != True:
+    g1.onePlay(p1, 1)
+    print(g1.displayBoard())
+    g1.onePlay(p2, 2)
+    print(g1.displayBoard())
+    g1.determineWin()
+
+if g1.board[0] > g1.board[13]:
+    print("Player 1 wins!")
+else:
+    print("Player 2 wins!")
 
 
